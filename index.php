d2b8877194cf517d391bd1c217a3c56458aedd6b
Hardik-redspark <55910184+Hardik-redspark@users.noreply.github.com>
2019-09-28 17:03:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,25 @@
 <?php
 
 echo "Hello";
+echo "<br>";
 
 echo "check"; //nirmal
-
+echo "<br>";
 echo "master";
-
+echo "<br>";
 echo "hello";
-
+echo "<br>";
 echo "samir bhogayta";
-
+echo "<br>";
 echo "hardik Meghnathi";
-
+echo "<br>";
 echo "nirmal patel";
-
+echo "<br>";
 echo "new updates by samir without pull"
-
+echo "<br>";
 echo "hello checl pull"; //nirmal
+echo "<br>";
 echo "update latest push"; /// by hardik
+echo "<br>";
 
 ?>
